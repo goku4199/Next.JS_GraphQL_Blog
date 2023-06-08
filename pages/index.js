f6650212b6e2a9d0ee69c0a1c5150ad96d8c50e7
goku$9914 <anshumaantiwari123@gmail.com>
2023-06-08 16:07:23 +0530

    first commit

---
+++ pages/index.js
@@ -0,0 +1,54 @@
+import Head from 'next/head'
+import Link from 'next/link';
+import {PostCard,Categories,PostWidget} from '../components'
+import { getPosts, getRecentPosts } from '@/services';
+import FeaturedPosts from '@/sections/FeaturedPosts';
+
+export default function Home({posts}){
+
+  
+  return (
+    <div className='container mx-auto px-10 mb-8'>
+      <Head>
+        <title>CMS BLOG</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <FeaturedPosts />
+      <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
+
+       <div className='lg:col-span-8 col-span-1'>
+
+        {posts.map((post, index) => (
+          <div>
+            <PostCard post={post.node} key={post.title}/> 
+          </div>
+        ))}
+
+       </div>
+
+       <div className='lg:col-span-4 col-span-1'>
+
+        <div className='lg:sticky relative top-8'>
+
+          <PostWidget />
+          <Categories />
+
+        </div>
+
+       </div>
+        
+
+      </div>
+    </div>
+  )
+}
+
+export async function getStaticProps(){
+  const posts = (await getPosts()) || [];
+
+  return {
+    props: {posts}
+  }
+}
+
